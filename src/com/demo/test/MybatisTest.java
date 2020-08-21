5d79b981a7b6fa2b10e7505c76ef4747bde08daa
xiapei <1063658474@qq.com>
2020-08-21 18:06:23 +0800

    *)mybatis缓存

--- src/com/demo/test/MybatisTest.java
+++ src/com/demo/test/MybatisTest.java
@@ -24,6 +24,24 @@ public class MybatisTest {
 
     private final SqlSessionFactory sqlSessionFactory = getSqlSession();
 
+    /**
+     * 一级缓存：是SqlSession级别的缓存；sqlSessionFactory.openSession()开启一个新的SqlSession
+     *  1、不同的SqlSession使用的是不同的一级缓存
+     *      只有在同一个SqlSession期间查询到的数据会保存这个SqlSession的缓存中
+     *      下次使用这个SqlSession查询会从缓存中取
+     *  2、同一个方法不同的参数，由于没有查询过，所以还是会发新的sql
+     *  3、在这个SqlSession期间执行过一次增删改操作会把缓存清空
+     *  4、手动清空一级缓存session.clearCache();
+     *
+     * 二级缓存：全局作用域级别的缓存；SqlSession关闭或者提交之后，一级缓存的数据会存放在二级缓存中；mybatis默认不使用
+     *  1、mybatis-config.xml文件 开启全局缓存开关<setting name="cacheEnabled" value="true"/>
+     *  2、相应的mapper.xml加入 使用二级缓存的标签<cache/>
+     *     是因为每一个mapper都有自己的二级缓存，二级缓存是namespace命名空间级别的
+     *  3、相应的bean实体类需要实现Serializable序列化类
+     *
+     * 缓存的查询顺序：二级缓存->一级缓存->数据库
+     */
+
     public SqlSessionFactory getSqlSession() {
         String resource = "conf/mybatis-config.xml";
         InputStream inputStream = null;
